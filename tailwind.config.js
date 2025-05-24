1e40c9e6b78cac2e5b1fc47a3ab1aad0e83f8527
Mateus Carvalho Grecco <greccoomateus@gmail.com>
2025-05-24 01:44:31 -0300

    Configuração do Tailwind CSS e adicionado arquivos de configuração

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // Diz ao Tailwind para procurar classes nos arquivos HTML na raiz
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
